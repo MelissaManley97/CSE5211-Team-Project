8c6ecd6b2046fa090ee6b97aa57d1710b30f730a
MelissaManley97 <mmanley2014@my.fit.edu>
2019-03-28 13:08:34 -0400

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,56 @@
+import sys
+import cProfile
+import io
+import pstats
+import time
+
+
+# Brute force pattern matching (left to right scan)
+# @param  text      The block of text
+# @param  pattern   The pattern to find within the text
+# @return Pair of the start and end indices of where in text the pattern is located
+# If the text is not found in the pattern, this function returns (-1,-1)
+def brute_force(text, pattern):
+    i = 0
+    n = len(text)
+    m = len(pattern)
+    j = 0
+    while i <= n-m:
+        # Left-right scan
+        while (j < m) and (pattern[j] == text[i+j]):
+            j += 1
+        # If pattern found, return the start/end indices of where
+        # in the text the pattern is located
+        if j == m:
+            return i, (i+j-1)
+        # Shift pattern one place in text to try again
+        i += 1
+        j = 0
+    #  Pattern not found
+    return -1, -1
+
+
+def main():
+    # Dummy test case for now
+    text_str = "abajdiasdabaababaaaaa"
+    pattern_str = "abaababa"
+
+    # TODO Open test file and read input
+    # For each test file in this directory: run it and print results
+    # Profile the three algorithms
+    # Start profiler
+    pr = cProfile.Profile()
+    pr.enable()
+    t = time.clock() # Start clock (more accurate time than profiler)
+    pattern_location = brute_force(text_str, pattern_str)
+    print(time.clock()-t) # Stop clock
+    # Stop profiling
+    pr.disable()
+    s = io.StringIO()
+    sortBy = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortBy)
+    ps.print_stats()
+    print(s.getvalue())
+
+if __name__ == "__main__":
+    main()
---
+++ MorrisPratt.py
@@ -0,0 +1,94 @@
+import sys
+import cProfile
+import io
+import pstats
+import time
+
+
+# Morris Pratt Strings will contain
+# the actual given string and its computed borders
+class MP_String:
+    def __init__(self, text):
+        self.string = text
+        self.border = compute_border(text)
+
+
+# Computes the borders for a given pattern
+# @param pattern   The text pattern
+# @return  List containing all the border lengths of the pattern
+def compute_border(pattern):
+    # Initialize borders array
+    border = [0 for i in range(len(pattern)+1)]
+    # First entry should always be -1
+    border[0] = -1
+    b = -1
+
+    # For each non-empty prefix
+    for j in range(1, len(pattern)+1):
+        # Find the length of the longest border of prefix pattern[0..(j-1)].
+        # Reduce the border until a match is found or no border exists
+        while b >= 0 and not (pattern[b] == pattern[j - 1]):
+            b = border[b]
+        # Extend the border by one character when next characters match
+        b += 1
+        border[j] = b
+    return border
+
+
+# Morris Pratt pattern matching
+# @param  text      The block of text
+# @param  pattern   The pattern to find within the text
+# @return Pair of the start and end indices of where in text the pattern is located
+# If the text is not found in the pattern, this function returns (-1,-1)
+def morris_pratt(text, pattern):
+    # Same as brute force except for how shifts are made when there is a mismatch
+    i = 0
+    n = len(text)
+    m = len(pattern.string)
+    j = 0
+    # Keep trying to find the pattern while the text index is legal
+    while i <= n - m:
+        # Left-right scan
+        while (j < m) and (pattern.string[j] == text[i + j]):
+            j += 1
+        # If pattern found, return the start/end indices of where
+        # in the text the pattern is located
+        if j == m:
+            return i, (i+j-1)
+        # Morris-Pratt Shift
+        i = i + j - pattern.border[j]
+        if 0 < pattern.border[j]:
+            j = pattern.border[j]
+        else:
+            j = 0
+    #  Pattern not found
+    return -1, -1
+
+
+def main():
+    # Dummy test case for now
+    text_str = "abajdiasdabaababaaaaa"
+    pattern_str = "abaababa"
+    pattern = MP_String(pattern_str)
+
+
+    # TODO Open test file and read input
+    # For each test file in this directory: run it and print results
+    # Profile the three algorithms
+    # Start profiler
+    pr = cProfile.Profile()
+    pr.enable()
+    t = time.clock() # Start clock (more accurate time than profiler)
+    # Run algorithm
+    pattern_location = morris_pratt(text_str, pattern)
+    print(time.clock()-t) # Stop clock
+    # Stop profiling
+    pr.disable()
+    s = io.StringIO()
+    sortBy = 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortBy)
+    ps.print_stats()
+    print(s.getvalue())
+
+if __name__ == "__main__":
+    main()
